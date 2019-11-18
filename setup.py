2db5b8371e521b742302f05c3bc5385b5bf950b5
Loc Nguyen <loc.nguyen14061996@gmail.com>
2019-11-18 15:13:33 +0700

    a

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Loc Nguyen',                   # Type in your name
   author_email = 'your.email@domain.com',      # Type in your E-Mail
   url = 'https://github.com/locnguyen14061996/bitk',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/locnguyen14061996/bitk/archive/0.0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/locnguyen14061996/bitk/archive/0.0.2.tar.gz',    # I explain this later on
   keywords = ['python3', 'simple','business intelligence'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas', 
